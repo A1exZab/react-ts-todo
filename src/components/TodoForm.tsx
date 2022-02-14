9e2bb1513ac16e2b8bd2a7a9157f78ea4c6ba38a
A1exZab <lisandrugrin@gmail.com>
2022-02-14 15:49:46 +0300

    Added Navbar&TodoForm components

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,25 @@
+import React, {useRef} from "react";
+
+export const TodoForm: React.FC = () => {
+  
+  const ref = useRef<HTMLInputElement>(null)
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter'){
+      console.log(ref.current!.value)
+      ref.current!.value=''
+    }
+  }
+
+  return (
+    <div className="input-field mt2">
+      <input 
+        ref={ref}  
+        type="text" 
+        id="title" 
+        placeholder="Введите название дела" 
+        onKeyPress={keyPressHandler}/>
+      <label htmlFor="title" className="active">Введите название дела</label>
+    </div>
+  )
+}
