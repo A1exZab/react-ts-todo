97efc46e09fdef0fa1d39d5036e1396c016e40f1
A1exZab <lisandrugrin@gmail.com>
2022-02-16 15:29:42 +0300

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+import { ITodo } from './interfaces';
 
 
 const App: React.FC = () =>  {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]')
+    setTodos(saved)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addHandler = (title: string) => {
-    console.log('Add new todo: ' + title)
+    const newTodo: ITodo = {
+      title:title,
+      id: Date.now(),
+      completed: false
+    }
+    setTodos(prev => [newTodo, ...prev])
+  }
+
+  const toggleHandler = (id: number) => {
+    setTodos(prev => prev.map(todo => {
+      if(todo.id === id) {
+        return {...todo, completed: !todo.completed}
+      }
+      return todo
+      
+    })
+    )
+  }
+
+  const removeHandler = (id: number) => {
+    const shouldRemove: boolean = window.confirm('Вы уверены?')
+    
+    if(shouldRemove){
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
+    
   }
 
   return (
@@ -16,6 +52,12 @@ const App: React.FC = () =>  {
       <div className='container'>
         
         <TodoForm onAdd={addHandler}/>
+        <TodoList 
+          todos={todos}
+          onToggle={toggleHandler}
+          onRemove={removeHandler}
+          />
+        
 
       </div>
     </>
