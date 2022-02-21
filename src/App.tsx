a2d9160ebbd457cec81594a3993cdf46dca87261
A1exZab <lisandrugrin@gmail.com>
2022-02-21 14:09:00 +0300

    Added interfaces folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
 import { TodoList } from './components/TodoList';
-import { ITodo } from './interfaces';
+import { ITodo } from './interfaces/interfaces';
 
 
 const App: React.FC = () =>  {
